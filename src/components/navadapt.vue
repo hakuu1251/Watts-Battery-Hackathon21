66bc9bccbb1303aaea21b458dbf35ed60e6375b5
hakuu1251 <hakuu1251@gmail.com>
2019-12-21 21:24:28 +0300

    Add adapt

---
+++ src/components/navadapt.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="nav-disp">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <img src="../assets/watts_battery_white_logo.png" class="d-inline-block align-top mt-1" alt="">
+
+            <b-navbar-toggle target="nav-collapse"/>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">Settings</b-nav-item>
+                    <b-nav-item href="index.html">SingOut</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navadapt"
+    }
+</script>
+
+<style scoped>
+    .nav-disp {
+        display: none;
+    }
+    @media (max-width: 590px) {
+        .nav-disp {
+            display: block;
+        }
+    }
+</style>
